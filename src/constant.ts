36c96cf417e789ccbaaf8146b070878454e8861c
mymiracle0118 <noahsflood908@gmail.com>
2024-06-14 11:40:11 +0200

    add asset type

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,4 @@
-import type { NFTToken } from "$lib/types.js";
+import type { NFTToken, Token } from "$lib/types.js";
 
 export const nftInfo: NFTToken = {
   name: "Founder's Pirate",
@@ -6,4 +6,12 @@ export const nftInfo: NFTToken = {
   id: 2163,
   address: "0x1b41d54b3f8de13d58102c50d7431fd6aa1a2c48",
   standard: "ERC721"
+}
+
+export const tokenInfo: Token = {
+  name: "Bitcoin",
+  symbol: "BTC",
+  totalSupply: 100,
+  decimal: 18,
+  address: "0x1b41d54b3f8de13d58102c50d7431fd6aa1a2c48"
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -175,6 +175,7 @@ export interface NFTAttribute {
   value: string, // value
 }
 
+// Token ---------------------------------------------------------------------------------------------------------------------------------------
 // NFT 
 export interface NFTMetadata {
   platform: string, // nft website
@@ -191,7 +192,7 @@ export interface NFTMetadata {
   attributes: NFTAttribute[]
 }
 
-// Token
+// NFT Token
 export interface NFTToken {
   name: string, // token name
   symbol: string, // token symbol
@@ -199,3 +200,35 @@ export interface NFTToken {
   address: string, // nft contract address
   standard: string // token type
 }
+
+export interface AssetExtra {
+  authorized: number,
+  authorized_to_maintain_liabilities: number,
+  unauthorized: number
+}
+
+export interface AssetFlag {
+  auth_required: boolean,
+  auth_revocable: boolean,
+  auth_immutable: boolean
+}
+
+// FT Token
+export interface Asset {
+  asset_type: string, // This asset's type. Either credit_alphanum4 or credit_alphanum12.
+  asset_code: string, // This asset's code
+  asset_issuer: string, // The Stellar address of this asset’s issuer.
+  accounts: AssetExtra, // The number of accounts grouped by each trustline flag state.
+  num_claimable_balances: number, // The current number of claimable_balances for this asset.
+  num_contracts: number, // The current number of Soroban contracts holding this asset.
+  num_liquidity_pools: number, // The current number of liquidity pools holding this asset.
+  balances: AssetExtra, // The number of units issued for this asset grouped by each trustline flag state.
+  claimable_balances_amount: string, // The number of units for this asset held by all claimable balances.
+  contracts_amount: string, // The number of units for this asset held by all Soroban contracts.
+  liquidity_pools_amount: string, // The number of units for this asset held by all liquidity pools.
+  amount: string, // The number of authorized units issued for this asset. This will be deprecated in Horizon v3.
+  num_accounts: number, // The number of accounts that have issued a trustline to this asset. If the auth_required flag for this asset's issuer is set to true, this number only includes the accounts who have both set up a trustline to the asset and have been authorized to hold the asset. This will be deprecated in Horizon v3.
+  flags: AssetFlag
+}
+
+// Token Ends ---------------------------------------------------------------------------------------------------------------------------------------
