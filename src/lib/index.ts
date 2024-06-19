1e653dd09894a300de2e1031a7271c54da7f9f16
codepert <trang.uixdev@gmail.com>
2024-06-19 15:55:23 +0900

    exporting components.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,8 @@
 // Reexport your entry components here
+// Reexport your entry components here
+export { default as Card } from "./card/card.svelte";
+export { default as CardItem } from "./card/item.svelte";
+export { default as AssetSelector } from "./assetSelector/assetSelector.svelte";
+export { default as ImagePoster } from "./imagePoster/imagePoster.svelte";
+export { default as NftPoster } from "./nftPoster/nftPoster.svelte";
+export { default as YoutubePoster } from "./youtubePoster/youtubePoster.svelte";
