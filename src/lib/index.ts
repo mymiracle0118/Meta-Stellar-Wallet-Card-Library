3396c307e68f083ee4b9669e0ca36dd54138d923
Paul Fears <paulrfears@gmail.com>
2024-06-19 21:55:42 -0500

    export components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
 // Reexport your entry components here
+import Card from './card/card.svelte';
+import CardItem from "./card/item.svelte";
+import ImagePost from "./imagePoster/imagePoster.svelte";
+import nftPost from './nftPoster/nftPoster.svelte';
+
+export {Card, CardItem, ImagePost, nftPost}
