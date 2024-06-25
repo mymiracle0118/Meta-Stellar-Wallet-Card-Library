f4d76ae65bb903f1d8564097593c30198616888d
Paul Fears <paulrfears@gmail.com>
2024-06-24 23:05:05 -0500

    exported normalize funcs

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 // Reexport your entry components here
 
-import { openCanvasModal } from './CanvasModal/index.js';
+import { normalizeData, renderCanvas } from './CanvasModal/index.js';
 export { default as Card } from "./card/card.svelte";
 export { default as AssetSelector } from "./assetSelector/assetSelector.svelte";
 export { default as ImagePoster } from "./imagePoster/imagePoster.svelte";
@@ -9,6 +9,6 @@ export { default as YoutubePoster } from "./youtubePoster/youtubePoster.svelte";
 export { default as CanvasModal } from "./CanvasModal/Modal.svelte";
 
 export type * from "./types.js";
-export { openCanvasModal };
+export { normalizeData,  renderCanvas};
 
 
