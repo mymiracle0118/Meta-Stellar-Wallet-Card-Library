2f50d78978b53c359fee0b0168ac31fd2fe8225e
Paul Fears <paulrfears@gmail.com>
2024-06-24 20:30:05 -0500

    export update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,6 +5,7 @@ import ImagePost from "./imagePoster/imagePoster.svelte";
 import NFTPost from './nftPoster/nftPoster.svelte';
 import YTPost from './nftPoster/nftPoster.svelte';
 import TokenPost from './tokenPoster/tokenPoster.svelte';
+import { openCanvasModal } from './CanvasModal/index.js';
 import type { StellarAsset, walletInfo, DispAsset, AssetMetaData, } from './types.js';
 export {
     Card, 
@@ -13,6 +14,7 @@ export {
     NFTPost, 
     YTPost,
     TokenPost,
+    openCanvasModal,
 }
 
 export type{
