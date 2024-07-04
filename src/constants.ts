3bf75d79c5254de82e8eb293f570d08d7a52b696
mymiracle0118 <noahsflood908@gmail.com>
2024-07-04 11:26:53 +0200

    add tokePost card

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,16 @@
 import type { AssetAccount } from "$lib/types.js";
 
-// export const assetInfo: AssetAccount = {
+// export const nftInfo: AssetAccount = {
 //   code: "Spunks",
 //   issuer: "GCAR7OJULRZBOMMUPJ33YMI3O6GPBMKHWVWQAD2BHSH6NZ25QYMI44UP"
 // }
 
-export const assetInfo: AssetAccount = {
+export const nftInfo: AssetAccount = {
   code: "CYBERMAN007",
   issuer: "GABSMRCVPJJQZ3PDYKR4B3UCU26JBMJFSWEQVQAHPDEFHDMSJI3IQ7IT"
+}
+
+export const tokenInfo: AssetAccount = {
+  code: "BTCLN",
+  issuer: "GDPKQ2TSNJOFSEE7XSUXPWRP27H6GFGLWD7JCHNEYYWQVGFA543EVBVT"
 }
--- src/lib/tokenPoster/tokenPoster.svelte
+++ src/lib/tokenPoster/tokenPoster.svelte
@@ -1,62 +1,87 @@
 <script lang="ts">
 	import type {  ComponentProps} from 'svelte';
   import { ImagePlaceholder } from 'flowbite-svelte';
-
   import { onMount } from 'svelte';
   import { twMerge } from 'tailwind-merge';
+
 	import Frame from "../frame/frame.svelte";
-	import type {SizeType} from '$lib/types.js';
-  import type {NFTToken, NFTMetadata} from '$lib/types.js';
-	import {sizes} from '$lib/constants.js';
+	import type {AssetAccount, AssetRaw, AssetStatistics, AssetFlag, AssetMetaData, links, SizeType} from '$lib/types.js';
+  import {getMetadata} from '$lib/utility.js';
 
 	let showModal = false;
 
-  export let token: NFTToken | undefined = undefined;
-  export let nft_metadata: NFTMetadata | undefined = undefined;
-  export let metadata_url: string;
-  export let imgClass: string |undefined = undefined;
+  export let assetAccount: AssetAccount;
+  export let imgClass: string | undefined = undefined;
+  export let padding: SizeType | 'none' = 'lg';
 
 	interface $$Props extends ComponentProps<Frame> {
-    token: NFTToken;
-    metadata_url: string;
+    assetAccount: AssetAccount;
     imgClass?:string;
+    padding?: SizeType | 'none';
 	}
 
+  const paddings: Record<SizeType | 'none', string> = {
+    none: '',
+    xs: 'p-2',
+    sm: 'p-4',
+    md: 'p-4 sm:p-5',
+    lg: 'p-4 sm:p-6',
+    xl: 'p-4 sm:p-8'
+  };
+
+  const yPaddings: Record<SizeType | 'none', string> = {
+    none: '',
+    xs: 'py-2',
+    sm: 'py-4',
+    md: 'py-4 sm:py-5',
+    lg: 'py-4 sm:py-6',
+    xl: 'py-4 sm:py-8'
+  };
+
+  let innerPadding: string;
+  $: innerPadding = paddings[padding];
+  let innerYpadding: string;
+  $: innerYpadding = yPaddings[padding];
+
+  let assetInfo: AssetRaw;
+  let assetMetadata: AssetMetaData;
+
+  async function getAssetData(assetAccount: AssetAccount) {
+    const data = await getMetadata(assetAccount);
+    if(data?.result) {
+      assetInfo = data.data?.asset_raw as AssetRaw;
+      assetMetadata = data.data?.metadata as AssetMetaData;
+      console.log("token card metadata", assetMetadata);
+    }
+  }
+  
+  onMount(() => {
+    getAssetData(assetAccount);
+	});
 
   let cardClass: string;
   $: cardClass = twMerge('flex w-full', $$props.class);
+  $: cardClass = twMerge('flex w-full',  $$props.class, innerPadding, $$restProps.imgHoverTransform && 'img-hover', $$restProps.hoverTransform && 'hover');
 
 
 	let imgCls:string;
 	$:imgCls = twMerge('hover:cursor-pointer', imgClass)
-
-  async function getMetadata (metadata_url: string) {
-		if (metadata_url == undefined) return;
-
-		try {
-			const res = await fetch(metadata_url);
-      if(res.ok){
-        const result =  await res.json();
-        nft_metadata = result as NFTMetadata;
-        console.log(nft_metadata);
-      }
-		} catch (e) {
-			console.log('nft metadata request error', e);
-		}
-	};
-  
-  onMount(() => {
-    getMetadata(metadata_url);
-	});
-
 </script>
 
 <Frame tag="div" {...$$restProps} class={cardClass} on:click={() => (showModal = true)}>
-  {#if nft_metadata?.image_url}
-  <img class={imgCls} src={nft_metadata?.image_url} alt="product 1" />
+  {#if assetMetadata?.image}
+  <img class={imgCls} src={assetMetadata?.image} alt="product 1" />
   {:else}
-  <ImagePlaceholder imgOnly class="mt-12" />
+  <!-- <ImagePlaceholder imgOnly class="mt-12 w-full" style={{width:'100%'}} size="lg"/> -->
+  <div class="flex justify-center items-center w-full h-48 bg-gray-300 rounded dark:bg-gray-700">
+    <svg width="48" height="48" class="text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512">
+      <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
+    </svg>
+  </div>
   {/if}
-  <div class="px-5 pb-5">
-  <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{token?.name + " #" + token?.id}</h5>
+  <p>{assetMetadata?.code ? assetMetadata?.code : ""}</p>
+  <p>{assetMetadata?.name ? assetMetadata?.name : ""}</p>
+  <p>{assetMetadata?.issuer ? assetMetadata?.issuer: ""}</p>
+  
+  <!-- <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{token?.name + " #" + token?.id}</h5> -->
 </Frame>
